3de98409d9b411f2f51c7200a4605a06238c8193
KaCode90 <kacode90@gmail.com>
2026-09-18 18:01:46 +0700

    6th Changes

--- fix_alert.py
+++
@@ -1,8 +0,0 @@
-﻿import re
-with open('static/app.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = re.sub(r'onclick="alert\(\'(.*?)\'\)"', r'onclick="alert(&quot;\1&quot;)"', content)
-
-with open('static/app.js', 'w', encoding='utf-8') as f:
-    f.write(content)
--- temp.py
+++
@@ -1,1 +0,0 @@
-﻿import sqlite3; db=sqlite3.connect('backend/data.sqlite'); print(db.execute('SELECT name FROM sqlite_master').fetchall())
